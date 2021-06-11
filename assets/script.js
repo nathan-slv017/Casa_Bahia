971e5416299c9e3c4921320a9f653848664f4cad
Nathan da Silva Barrozo <79716129+nathan-slv017@users.noreply.github.com>
2021-06-10 22:08:12 -0300

    feito de hoje

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ function login() {
     document.querySelector(".login").addEventListener("mouseover", () => {
         document.querySelector(".fized").classList.remove("display")
     });
-    document.querySelector(".fized").addEventListener("mouseover", () => {
+    document.querySelector(".login").addEventListener("mouseover", () => {
         document.querySelector(".fized").classList.remove("display")
     });
     document.querySelector(".login").addEventListener("mouseout", () =>{
@@ -14,5 +14,22 @@ function login() {
         document.querySelector(".fized").classList.add("display")
     });
 }
+let onOf = 0;
+setInterval(() =>{
+    if( onOf === 1){
+        document.querySelector(".cartaoCasasBahia").innerHTML = `<p>30X SEM JUROS</p>`
+        onOf --;
+        return;
+    }
 
-login()
+    if(onOf === 0){
+        document.querySelector(".cartaoCasasBahia").innerHTML = `<p> CARTÃO CASAS BAHIA </p>`
+        onOf ++;
+        return;
+    }
+}, 1600);
+login();
+
+function imagen(n){
+    document.querySelector(".fotoPrincipal").innerHTML = `<img src="imagens/fotoIphone${n}.webp" alt="" width="400px">`
+}
